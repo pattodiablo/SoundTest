7e41907f4dc2c531fce90a584cd6cc617d14c827
okv94 <57909521+okv94@users.noreply.github.com>
2019-11-21 18:01:03 -0500

    updateo

--- ejemplo2ToneJS/ejmplo2.js
+++ ejemplo2ToneJS/ejmplo2.js
@@ -1,2 +1,14 @@
-const synth = new Tone.MembraneSynth().toMaster();
-synth.triggerAttackRelease("C2", "8n");
+(function() {
+  // Membrane Synth https://tonejs.github.io/docs/r12/MembraneSynth
+  const synth = new Tone.MembraneSynth().toMaster();
+  function playSynth() {
+    synth.triggerAttackRelease("C2", "8n");
+  }
+
+  /**
+   * Play Controls
+   */
+  document.querySelector("body").addEventListener("click", function() {
+    playSynth();
+  });
+})();
